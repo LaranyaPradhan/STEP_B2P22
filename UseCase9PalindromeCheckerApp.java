abf3463e830018c9649f0e23a8abea9d33376dc7
LaranyaPradhan <laranya011@gmail.com>
2026-03-09 15:35:10 +0530

    Add files via upload

---
+++ UseCase9PalindromeCheckerApp.java
@@ -0,0 +1,25 @@
+public class UseCase9PalindromeCheckerApp {
+
+    public static boolean isPalindrome(String str, int start, int end) {
+        if (start >= end) return true;
+
+        if (str.charAt(start) != str.charAt(end))
+            return false;
+
+        return isPalindrome(str, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+
+        String input = "racecar";
+
+        boolean result = isPalindrome(input, 0, input.length() - 1);
+
+        System.out.println("Input String : " + input);
+
+        if (result)
+            System.out.println("Result : It is a Palindrome.");
+        else
+            System.out.println("Result : It is NOT a Palindrome.");
+    }
+}
