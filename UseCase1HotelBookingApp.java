b44b74187c45890c2403a3d4ce9afabd556c6ef6
LaranyaPradhan <laranya011@gmail.com>
2026-03-28 18:18:43 +0530

    Add files via upload

---
+++ UseCase1HotelBookingApp.java
@@ -0,0 +1,27 @@
+/**
+ * UseCase1HotelBookingApp
+ * 
+ * This class represents the entry point of the Hotel Booking Management System.
+ * It demonstrates how a Java application starts execution and displays
+ * a welcome message along with application details.
+ * 
+ * @author Laranya
+ * @version 1.0
+ */
+
+public class UseCase1HotelBookingApp {
+
+    /**
+     * Main method - entry point of the application
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("=======================================");
+        System.out.println("   Welcome to Book My Stay Application  ");
+        System.out.println("         Hotel Booking System           ");
+        System.out.println("              Version 1.0               ");
+        System.out.println("=======================================");
+
+    }
+}
